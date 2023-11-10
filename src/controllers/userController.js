e34de9b8559d3cb287c353e66bf2b71ea566638f
nicocabrerafit2 <nicocabrera8@outlook.com>
2023-11-10 00:48:02 -0300

    empiezo a trabajar en controller de usuario, llamo al modelo User, y el acceso a unas vistas basicas

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,153 +1,31 @@
-const fs = require("fs");
-const path = require("path");
-const bcrypt = require("bcryptjs");
-const { validationResult } = require("express-validator");
+const { User } = require("../database/models");
 
 module.exports = {
-  home: (req, res) => {
-    res.resdirect("home", {
-      user: req.session.userLogged,
-    });
-  },
   login: (req, res) => {
-    return res.render("userLogin", {});
+    return res.render("userLogin");
   },
+
   register: (req, res) => {
-    return res.render("userRegister", {});
+    return res.render("userRegister");
   },
 
   list: function (req, res) {
-    res.render("userList", { users: users, user: req.session.userLogged });
+    res.render("userList");
   },
 
-  detail: (req, res) => {
-    db.usuarios
-      .findByPk(req.params.usuarioID, {
-        include: [{ association: "carritoProducto" }],
-      })
-      .then(function (data) {
-        let users = data.dataValues;
-        res.render("usersDetail", { users, user: req.session.userLogged });
+  detail: async (req, res) => {
+    try {
+      const userFinded = await User.findByPk(req.params.userID, {
+        include: [{ association: "carts" }],
       });
+      res.render("usersDetail");
+    } catch {
+      res.render("error404");
+    }
   },
 
   logout: (req, res) => {
     req.session.destroy();
     return res.redirect("/");
   },
-
-  procesLogin: (req, res) => {
-    let errores = validationResult(req);
-    if (errores.isEmpty()) {
-      for (let i = 0; i < users.length; i++) {
-        if (users[i].email == req.body.emailLogin) {
-          if (
-            bcrypt.compareSync(req.body.contrasenaLogin, users[i].contrasena)
-          ) {
-            var usuarioALogearse = users[i];
-          }
-        }
-      }
-      if (usuarioALogearse == undefined) {
-        return res.render("errorLogin", { user: req.session.userLogged });
-      } else {
-        req.session.userLogged = usuarioALogearse;
-        if (req.body.recuerdame != undefined) {
-          res.cookie("recuerdame", usuarioALogearse.emailLogin, {
-            maxAge: 60000,
-          });
-        }
-        return res.render("home", {
-          user: req.session.userLogged,
-        });
-      }
-    } else {
-      res.render("errorLogin", {
-        errores: errores.array(),
-        user: req.session.userLogged,
-      });
-    }
-  },
-
-  // Create -  Method to store
-
-  store: (req, res) => {
-    let errores = validationResult(req);
-    if (errores.isEmpty()) {
-      //el siguiente if es para cuando el json se encuentra vacio sin ningun usuario
-      if (users.length != 0) {
-        let fotoUsuario;
-        // seguir revisando para que se fije si el mail ya se encuentra registrado
-
-        for (let i = 0; i < users.length; i++) {
-          if (users[i].email == req.body.email) {
-            return res.render("register", {
-              errores: [
-                {
-                  msg: "Este email ya está registrado",
-                },
-              ],
-              user: req.session.userLogged,
-            });
-          }
-        }
-        // --------------------------------- hasta aca ---------------------
-        if (req.file != undefined) {
-          fotoUsuario = req.file.filename;
-        } else {
-          fotoUsuario = "default.png";
-        }
-
-        let newUser = {
-          usuarioID: users[users.length - 1].usuarioID + 1,
-          nombre: req.body.nombre,
-          email: req.body.email,
-          contrasena: bcrypt.hashSync(req.body.contrasena, 10),
-          fotoUsuario,
-          //carritoProductoID: users[users.length - 1].usuarioID + 1,
-        };
-
-        users.push(newUser);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
-        db.usuarios
-          .create(newUser)
-          .then((storedUser) => {
-            return res.redirect("/users/login");
-          })
-          .catch((error) => console.log(error));
-      } else {
-        if (req.file != undefined) {
-          fotoUsuario = req.file.filename;
-        } else {
-          fotoUsuario = "default.png";
-        }
-
-        let newUser = {
-          usuarioID: 1,
-          nombre: req.body.nombre,
-          email: req.body.email,
-          contrasena: bcrypt.hashSync(req.body.contrasena, 10),
-          fotoUsuario,
-          //carritoProductoID: users[users.length - 1].usuarioID + 1,
-        };
-        users.push(newUser);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
-        db.usuarios
-          .create(newUser)
-          .then((storedUser) => {
-            return res.redirect("/users/login");
-          })
-          .catch((error) => console.log(error));
-      }
-    } else {
-      res.render("register", {
-        errores: errores.array(),
-        user: req.session.userLogged,
-      });
-    }
-  },
-  // Agregar metodo para borrar usuario (eliminar cuenta, borrado logico)
-  // Agregar metodo para modificar usuario (modificar datos)
-  // Agregar metodo para buscar usuario
-  // Agregar metodo para ir al perfil de l usuario
 };
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,97 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const path = require("path");
-const { body } = require("express-validator");
-const userController = require("../controllers/userController");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "../Mercadito-liebre/public/img/imgUsers");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-const validacionesRegistro = [
-  body("nombre")
-    .notEmpty()
-    .isLength({ min: 2 })
-    .withMessage("Debe introducir su nombre de mas de 2 caracteres"),
-  body("email")
-    .notEmpty()
-    .isEmail()
-    .withMessage("Debe introducir un mail válido (ejemplo@email.com)"),
-  body("contrasena")
-    .notEmpty()
-    .isLength({ min: 8 })
-    .withMessage("Debe introducir una contraseña de 8 o mas caracteres"),
-  body("fotoUsuario").custom(async (value, { req }) => {
-    let file = req.file;
-    let acceptedExtensions = [".jpg", ".png", ".jpeg"];
-
-    if (!file) {
-      throw new Error(
-        "Tienes que elegir una imagen en alguno de estos formatos jpg-png-jpeg"
-      );
-    } else {
-      let fileExtension = path.extname(file.originalname);
-      if (!acceptedExtensions.includes(fileExtension)) {
-        throw new Error(
-          `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
-            ", "
-          )}`
-        );
-      }
-    }
-
-    return true;
-  }),
-];
-
-const validacionesLogin = [
-  body("emailLogin") // **** Agregar validacion que el mail ya esta registrado para poder entrar ****
-    .notEmpty()
-    .isEmail()
-    .withMessage("Debe introducir un mail válido (ejemplo@email.com)"),
-  body("contrasenaLogin")
-    .notEmpty()
-    .isLength({ min: 8 })
-    .withMessage("Debe introducir una contraseña de 8 o mas caracteres"),
-];
-
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    fileLocal = file;
-    if (
-      file.mimetype == "image/png" ||
-      file.mimetype == "image/jpg" ||
-      file.mimetype == "image/jpeg"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  },
-});
 
 /* CREATE USER */
 router.get("/register", userController.register);
-router.post(
-  "/",
-  upload.single("imagen"),
-  validacionesRegistro,
-  userController.store
-);
+
 /* LOGIN USER */
 router.get("/login", userController.login);
-router.post("/login", validacionesLogin, userController.procesLogin);
+router.post("/login", userController.procesLogin);
 
 /* LOGOUT USER */
 router.get("/logout", userController.logout);
 
 /* DETAIL USER */
-router.get("/detail/:usuarioID", userController.detail);
+router.get("/detail/:userID", userController.detail);
 
 /* LIST OF USERS */
 router.get("/userList", userController.list);
